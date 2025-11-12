c37b202a31f1a6f208689fb3507cd2c1e676e5f9
antoniaturcatto <antoniaturcatto29@gmail.com>
2025-11-12 20:43:20 -0300

    Cabeçalho para entrega

---
+++ main.c
@@ -0,0 +1,14 @@
+/*GRAFOMOOD
+Integrantes: AntoniaTurcatto, Tássia J. M. Machado, Kauany da Costa, Helen Muniz.
+
+Tema: Ligações entre Personagens
+Grafo orientado (pesos nas relações individualizadas)
+Tipo de representação usada: Lista de adjacência
+Percurso: BFS/DFS [a ver]
+Funcionalidades adicionais: Salvar em arquivo .dot, carregar grafo de arquivo, Conexão entre dois vértices...[a ver]
+Observações:
+*/
+
+int main(){
+    return 0;
+}
