1c73133da91898fbe28476bf780d3d1cdcba4763
antoniaturcatto <antoniaturcatto29@gmail.com>
2025-11-14 20:30:28 -0300

    Tratamento de input textual stdin e criação do grafo

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ Observações:
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <strings.h>
+#include <stdbool.h>
 
 #define MAX_NOME 75
 #define MAX_PERSONAGEM 20
@@ -42,6 +44,63 @@ typedef struct{
     PersonagNodo *raiz;
 }RedeConexao;
 
+RedeConexao* cria_rede();
+bool adiciona_personagem_rd(RedeConexao *rd);
+Personagem cria_personagem();
+
+///função para ler input textual do stdin tratando suas vulnerabilidades inerentes.
+bool ler_texto_stdin(char buffer[]);
+
+
+
+
 int main(){
     return 0;
+}
+
+RedeConexao* cria_rede(){
+    RedeConexao rede;
+    rede.quant_personagens = 0;
+    rede.raiz = NULL;
+    return &rede;
+}
+
+Personagem cria_personagem(){
+    Personagem p;
+    printf("Informe o nome do personagem [até %d caracteres]: ", MAX_NOME);
+    if (!ler_texto_stdin(p.nome)){
+        strcpy(p.nome, "Erro");
+    }
+    
+    printf("Informe a idade do personagem: ");
+    scanf("%hu", &p.idade);
+    
+    return p;
+}
+ 
+bool ler_texto_stdin(char buffer[]){
+    int c;
+    size_t len; 
+    if (fgets(buffer, MAX_NOME, stdin) == NULL) {
+        buffer[0] = '\0';
+        printf("\n");
+        return false; // erro ou EOF
+    }
+
+    len = strlen(buffer);
+    // se terminou com '\n', remove
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    } else {
+        // se não terminou com '\n', limpar o resto da linha do stdin
+        while ((c = getchar()) != '\n' && c != EOF);
+    }
+    printf("\n");
+    return true;
+}
+
+
+
+bool adiciona_personagem_rd(RedeConexao *rd){
+    //todo: fazer
 }
