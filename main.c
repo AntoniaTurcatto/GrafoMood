5fadc4364504a6ff46b54262efda67d26888528e
antoniaturcatto <antoniaturcatto29@gmail.com>
2025-11-14 22:24:31 -0300

    Implementação de inserção e structs base concluída

--- main.c
+++ main.c
@@ -32,27 +32,42 @@ typedef struct CONEC{
     struct CONEC *prox_conexao;//prox conexao do personagem de onde originou a primeira CONEC
 }Conexao;
 
+///Se houver apenas uma conexao: ult == NULL
+typedef struct{
+    unsigned short int quant_conex;
+    Conexao *prim;
+    Conexao *ult;
+}DescrConexoes;
+
 typedef struct PERS_NODO{
     int id_personagem;
     Personagem info;
     struct PERS_NODO *prox;//proximo da lista de adjacencia
-    Conexao *prim_conexao;
+    DescrConexoes desc_conexoes;
 }PersonagNodo;
 
+/// Caso exista apenas um personagem: ult_nodo == NULL
 typedef struct{
     unsigned short int quant_personagens;
+    unsigned int proximo_id;
     PersonagNodo *raiz;
+    PersonagNodo *ult_nodo;
 }RedeConexao;
 
 RedeConexao* cria_rede();
-bool adiciona_personagem_rd(RedeConexao *rd);
+
+bool adiciona_personagem_rd(RedeConexao *rd, Personagem pers);
+
+/// atualiza o vínculo entre personagens. Caso não exista o vínculo, cria.
+/// retorno: false caso não exista um dos personagens
+bool atualiza_vinculo(PersonagNodo *orig, unsigned int iddest, unsigned int peso, RedeConexao *rd);
+
 Personagem cria_personagem();
 
 ///função para ler input textual do stdin tratando suas vulnerabilidades inerentes.
 bool ler_texto_stdin(char buffer[]);
 
-
-
+bool personagem_valido(unsigned int id, RedeConexao *rd);
 
 int main(){
     return 0;
@@ -61,6 +76,7 @@ int main(){
 RedeConexao* cria_rede(){
     RedeConexao rede;
     rede.quant_personagens = 0;
+    rede.proximo_id = 0;
     rede.raiz = NULL;
     return &rede;
 }
@@ -101,6 +117,75 @@ bool ler_texto_stdin(char buffer[]){
 
 
 
-bool adiciona_personagem_rd(RedeConexao *rd){
-    //todo: fazer
+bool adiciona_personagem_rd(RedeConexao *rd, Personagem pers){
+    PersonagNodo *p_novop;
+    if(rd == NULL)
+        return false;
+
+    p_novop = (PersonagNodo*)malloc(sizeof(PersonagNodo)); 
+    p_novop->id_personagem = rd->proximo_id++; //incrementa após uso
+    p_novop->desc_conexoes.prim = NULL;
+    p_novop->desc_conexoes.prim = NULL;
+    p_novop->desc_conexoes.quant_conex = 0;
+    p_novop->prox = NULL;
+    p_novop->info = pers;
+    
+    switch (rd->quant_personagens){
+        case 0:
+            rd->raiz = p_novop;
+            break;
+        case 1:
+            rd->ult_nodo   = p_novop;
+            rd->raiz->prox = rd->ult_nodo;
+            break; 
+        default:
+            rd->ult_nodo->prox = p_novop;
+    }
+
+    rd->quant_personagens++;
+    return true;    
+}
+
+bool atualiza_vinculo(PersonagNodo *orig, unsigned int iddest, unsigned int peso, RedeConexao *rd){
+    bool dest_presente = false;
+    Conexao *connov;
+      
+    if(!personagem_valido(orig->id_personagem, rd) || !personagem_valido(iddest, rd)){
+        return false;
+    }
+
+    connov = (Conexao*)malloc(sizeof(Conexao));
+    connov->id_personagem = iddest;
+    connov->prox_conexao  = NULL; 
+    connov->peso          = peso;
+
+    switch(orig->desc_conexoes.quant_conex){
+        case 0:
+            orig->desc_conexoes.prim = connov;
+            break;
+        case 1: 
+            orig->desc_conexoes.ult = connov;
+            orig->desc_conexoes.prim->prox_conexao = orig->desc_conexoes.ult;
+            break;
+        default:
+            orig->desc_conexoes.ult->prox_conexao = connov;
+    }
+
+    orig->desc_conexoes.quant_conex++;
+    return true;
+}
+
+bool personagem_valido(unsigned int id, RedeConexao *rd){
+    bool presente = false;
+    PersonagNodo *pdest; 
+    
+    if(rd == NULL)
+        return false;
+
+    pdest = rd->raiz;
+    while(pdest != NULL && !presente){
+        presente = pdest->id_personagem == id;
+        pdest = pdest->prox;
+    }
+    return presente;
 }
